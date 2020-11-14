77dfcffca7b37b8828e1a94c4397030e2cd6e262
Drakx116 <alexandre2.guyot@gmail.com>
2020-11-14 15:09:09 +0100

    Fix /admin access and redirection pattern

--- src/Controller/Security/AdminController.php
+++ src/Controller/Security/AdminController.php
@@ -7,10 +7,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Class AdminController
+ * @package App\Controller\Security
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="admin_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -27,10 +32,9 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/logout", name="admin_logout")
      */
-    public function logout()
+    public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
